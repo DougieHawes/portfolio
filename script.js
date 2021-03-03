5b3de40bc74bf76011fecce78ee92932abca5d9f
dougiehawes <dougiehawes@hotmail.com>
2021-03-03 08:15:53 +0000

    added routing

---
+++ script.js
@@ -0,0 +1,47 @@
+let skills = document.querySelector(".skills");
+let work = document.querySelector(".work");
+let about = document.querySelector(".about");
+let contact = document.querySelector(".contact");
+
+let skillsLink = document.getElementById("skills");
+let workLink = document.getElementById("work");
+let aboutLink = document.getElementById("about");
+let contactLink = document.getElementById("contact");
+
+skills.style.display = "block";
+work.style.display = "none";
+about.style.display = "none";
+contact.style.display = "none";
+
+skillsLink.addEventListener("click", displaySkills);
+workLink.addEventListener("click", displayWork);
+aboutLink.addEventListener("click", displayAbout);
+contactLink.addEventListener("click", displayContact);
+
+function displaySkills() {
+  skills.style.display = "block";
+  work.style.display = "none";
+  about.style.display = "none";
+  contact.style.display = "none";
+}
+
+function displayWork() {
+  skills.style.display = "none";
+  work.style.display = "block";
+  about.style.display = "none";
+  contact.style.display = "none";
+}
+
+function displayAbout() {
+  skills.style.display = "none";
+  work.style.display = "none";
+  about.style.display = "block";
+  contact.style.display = "none";
+}
+
+function displayContact() {
+  skills.style.display = "none";
+  work.style.display = "none";
+  about.style.display = "none";
+  contact.style.display = "block";
+}
